341393acbcaf66640d5631e7fea46a4680cfa112
Bina87 <86130299+Bina87@users.noreply.github.com>
2021-08-04 16:35:00 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,39 +4,45 @@ import './App.css';
 export default class App extends Component {
   state = {
     person: {
-      fullName: 'sana',
-      bio: 'dfjkjfkj jfdkfdkn',
-      pro: 'web dev',
+      fullName: 'Sana Haj Sassi',
+      bio: 'There are few major things that you should never forget in the web developer bio. Paying attention to such things can help you in writing the best application and brightening up chances of getting desired job. Make sure that you discuss the gained skills in detail.',
+      profession: 'Web Developer',
       img: 'https://media.istockphoto.com/photos/smiling-confident-businesswoman-posing-with-arms-folded-picture-id1163294201?k=6&m=1163294201&s=612x612&w=0&h=mc8xTq-FAqDi1Ot3Ob6xpZ6wkm1EgCvK63huXtWCIzc=',
     },
     show: true,
-    count: 0,
+    count:0
+    
   };
-  handleShow = () => this.setState({show: !this.state.show});
-
-  handleCount = () => this.setState({count: this.state.count + 1});
+  personshow = () => this.setState({show: !this.state.show});
+  funct_count=()=>this.setState({count: this.state.count+1});
 
   componentDidMount() {
     console.log('start counting');
     setInterval(() => {
-      this.handleCount();
+      this.funct_count();
     }, 1000);
   }
+
+  
   render() {
     return (
       <div className="App">
-        <button onClick={this.handleShow}>
-          {this.state.show ? 'hide' : 'show'}{' '}
+        <div className="cadre">
+
+        <button onClick={this.personshow} className="button">
+          {this.state.show ? 'hide' : 'show'}
         </button>
         {this.state.show ? (
           <div>
-            <h1> Counter : {this.state.count} </h1>
+            <h5> Time : {this.state.count} </h5>
             <h1> {this.state.person.fullName} </h1>
-            <h1> {this.state.person.bio} </h1>
-            <h1> {this.state.person.pro} </h1>
-            <img src={this.state.person.img} alt="" />
+            <h3> {this.state.person.bio} </h3>
+            <h4 className="title"> {this.state.person.profession} </h4>
+            <img src={this.state.person.img} alt="web developer" width="350px" height="350px"/>
           </div>
         ) : null}
+        </div>
+
       </div>
     );
   }
